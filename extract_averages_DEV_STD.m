92db373fc5c4fea9e4f192a4bcccc0c07334d07f
AnneSo <as_dub@hotmail.com>
2022-09-26 15:35:28 +0200

    Update extract_averages_DEV_STD.m
    Remove unecessary commented lines

--- extract_averages_DEV_STD.m
+++ extract_averages_DEV_STD.m
@@ -1,5 +1,4 @@
 function [DEV1_subj, DEV2_subj,STD1_subj, STD2_subj, timepoints] = extract_averages_DEV_STD(INDIR,subjects)
-%function [DEV1_subj, DEV2_subj,STD1_subj, STD2_subj, grd_STD1, grd_STD2, grd_DEV1, grd_DEV2, diff_DEV1_STD1, diff_DEV2_STD2, mean_STD1_STD2, timepoints] = extract_averages_DEV_STD(INDIR,subjects)
 
 % Loop through all of the subjects in the study to create the dataset
 for loopnum = 1:length(subjects) %for each subject
@@ -23,26 +22,6 @@ for loopnum = 1:length(subjects) %for each subject
     DEV2_subj(loopnum,:,:)  = mean(EEG_dev2.data,3) ; 
 
 end
-
-% chan_indices = {'Fp1','Fp2','F4','Fz','F3','T7','C3','Cz','C4','T8','P4','Pz','P3','O1','Oz','O2','Lmon','Ref','Rmon','Left','Right'}
-% 
-% for chan_ind = 1:length(chan_indices)
-%     grd_STD1 = squeeze(mean(mean(STD1_subj(:,chan_ind,:),1),2)) ; 
-%     grd_STD2 = squeeze(mean(mean(STD2_subj(:,chan_ind,:),1),2)) ;
-%     grd_DEV1 = squeeze(mean(mean(DEV1_subj(:,chan_ind,:),1),2)) ; 
-%     grd_DEV2 = squeeze(mean(mean(DEV2_subj(:,chan_ind,:),1),2)) ; 
-%     
-% %Difference DEV1-STD1
-% diff_DEV1_STD1 = grd_DEV1 - grd_STD1;
-% 
-% %Difference DEV2-STD2 for grpA
-% diff_DEV2_STD2 = grd_DEV2 - grd_STD2;
-% 
-% %Mean STD1+STD2 for grpA
-% mean_STD1_STD2 = (grd_STD1 + grd_STD2)/2;
-% 
-% end
-
 
 timepoints = EEG_std1.times;
 
