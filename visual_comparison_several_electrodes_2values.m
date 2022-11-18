e2def49276ef9706e434865d6462277b5f1989fe
estellerv <estelle.herve@univ-amu.fr>
2022-11-18 14:51:32 +0100

    Update visual_comparison_visual_electrodes
    - added a new input 'STD_number' to the function that determines if the imported datasets contain all STD (unbalanced number of trials) or a balanced number of trials. If STD_number =1, balanced number or STD, if STD_number = 2, unbalanced number of STD (all STD)
    - modified legend and title so only one legend is displayed for all plots and a common title is added

--- visual_comparison_several_electrodes_2values.m
+++ visual_comparison_several_electrodes_2values.m
@@ -1,5 +1,5 @@
 %function visualisation = visual_comparison_several_electrodes_2values(mean_grpA, mean_grpB,raw_avg1_grpA,raw_avg2_grpA, raw_avg1_grpB,raw_avg2_grpB,timepoints,fig_name,legend1,legend2)
-function visualisation = visual_comparison_several_electrodes_2values(elec_to_disp_labels,elec_indices,mean_grpA, mean_grpB,timepoints,fig_name,legend1,legend2)
+function visualisation = visual_comparison_several_electrodes_2values(elec_to_disp_labels,elec_indices,mean_grpA, mean_grpB,timepoints,fig_name,legend1,legend2,STD_number)
 
 visualisation = figure('Name', fig_name,'Units','normalized','Position',[0,0,1,1]);
 
@@ -23,8 +23,8 @@ for elec_letter=1:size(elec_to_disp_labels,1)
         grd_avg_grpB = squeeze(mean(mean_grpB(idx_elec,:),1)) ; 
                 
         %Add 10 Hz lowpass filter for display
-        grd_avg_grpA = smooth(timepoints, grd_avg_grpA,span); %1000 points moving average (ex from pop_smootherp in ERPLAB)
-        grd_avg_grpB = smooth(timepoints, grd_avg_grpB,span);
+        %grd_avg_grpA = smooth(timepoints, grd_avg_grpA,span); %1000 points moving average (ex from pop_smootherp in ERPLAB)
+        %grd_avg_grpB = smooth(timepoints, grd_avg_grpB,span);
         %grd_avg_grpA = lowpass(grd_avg_grpA,10,16384);
         %grd_avg_grpB = lowpass(grd_avg_grpB,10,16384);
         %grd_avg_grpA_filt = pop_eegfilt(grd_avg_grpA, 0, 10, 8);
@@ -34,8 +34,8 @@ for elec_letter=1:size(elec_to_disp_labels,1)
         %todo
         
         % Plot timeseries
-        plot(timepoints,grd_avg_grpA_filt,'Color', grpA_color,'Linewidth',1.5); hold on ;set(gca,'YDir','reverse') ; 
-        plot(timepoints,grd_avg_grpB_filt,'Color', grpB_color,'Linewidth',1.5);  hold on; set(gca,'YDir','reverse') ;
+        plot(timepoints,grd_avg_grpA,'Color', grpA_color,'Linewidth',1.5); hold on ;set(gca,'YDir','reverse') ; 
+        plot(timepoints,grd_avg_grpB,'Color', grpB_color,'Linewidth',1.5);  hold on; set(gca,'YDir','reverse') ;
         
         % Plot transparetn halo (+-mad)
         %plotHaloPatchMAD(hAxes, timepoints, squeeze(mean((raw_avg1_grpA(:,idx_elec,:)+raw_avg2_grpA(:,idx_elec,:))/2,1)), [0,255,0]) ; 
@@ -45,13 +45,24 @@ for elec_letter=1:size(elec_to_disp_labels,1)
          %xlim([grd_STD_grpA.xmin, grd_STD_grpB.xmax]*1000); grid on ; 
          grid on;
          ylim([-4 5]);
-         legend(legend1,legend2);
          title(elec_to_disp_labels(elec_letter,elec_numb));
          xlabel('Times (ms)'); ylabel('uV');
          set(hAxes,'Fontsize',12);
-                
-         % To save data in vectoriel
-         %print('-dsvg',fullfile(INDIR,strcat(subjects{jj},'_',conditions{cc+1},'.svg'))) ; 
+                 
+    end
+    
+    %Add a single legend for 6 plots
+    fig = gcf;
+    fig.Position(3) = fig.Position(3) + 250;
+    Lgnd = legend(legend1,legend2,'Location','bestoutside');
+    Lgnd.Position(1) = 0.06;
+    Lgnd.Position(2) = 0.8;
+    
+    %Add a single title for 6 plots
+    if STD_number == 1
+        sgtitle('Balanced number of STDs', 'Fontsize', 16, 'FontWeight', 'bold');
+    elseif STD_number == 2
+        sgtitle('Unbalanced number of STDs (all)', 'Fontsize', 16, 'FontWeight', 'bold')
     end
     
 end
