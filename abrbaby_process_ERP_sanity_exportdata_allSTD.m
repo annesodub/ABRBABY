d0f536c54630f0ab0d11099bbee5edbb69a0b45e
estellerv <estelle.herve@univ-amu.fr>
2022-11-18 14:57:38 +0100

    Update abrbaby_process_ERP_sanity_exportdata_allSTD.m
    - added a new input chan_dir to the function so the channel location file is now indicated in the personalized script call
    - added a new input STD_number to the function that determines the number of STD (balanced or unbalanced)
    - ths STD_number has an impact on the rejection part of the script (not the same command lines depending on STD_number value) --> this new version allows to merge abrbaby_process_ERP_sanity_exportdata.m and abrbaby_process_ERP_sanity_exportdata_allSTD.m in a single function
    - a single legend and a common title is now displayed on the figure
    - figures are exported in .svg and .png formats. the .png is saved in a separate folder, whose path is in the variable 'plots_dir'. this new variable is added to the inputs of the function and must now be specified in personalized the script call

--- abrbaby_process_ERP_sanity_exportdata_allSTD.m
+++ abrbaby_process_ERP_sanity_exportdata_allSTD.m
@@ -1,6 +1,18 @@
-function [] = abrbaby_process_ERP_sanity_exportdata_allSTD(eeglab_path, biosig_installer_path,indir) 
+function [] = abrbaby_process_ERP_sanity_exportdata_allSTD(eeglab_path, biosig_installer_path,indir,plots_dir,chan_dir,STD_number) 
 % ERPs sanity check script - 
 % Estelle Herve, A.-Sophie Dubarry - 2022 - %80PRIME Project
+
+%----------------------------------------------------------------------------------------------------
+% %WHEN NOT IN 'FUNCTION' MODE:
+% eeglab_path = '\\Filer\home\Invites\hervé\Mes documents\MATLAB\eeglab2021.1';
+% biosig_installer_path = '\\Filer\home\Invites\hervé\Mes documents\MATLAB\eeglab2021.1\plugins\Biosig3.7.9\biosig_installer.m';
+% chan_dir ='\\Filer\\home\\Invites\\hervé\\Mes documents\\MATLAB\\eeglab2021.1\\plugins\\dipfit\\standard_BEM\\elec\\standard_1005.elc';
+% plots_dir = '\\Filer\home\Invites\hervé\Mes documents\These\EEG\Data\png_plots_eeg_data';
+% indir = '\\Filer\home\Invites\hervé\Mes documents\These\EEG\Data\DEVLANG_data';
+% %indir = '\\Filer\home\Invites\hervé\Mes documents\These\EEG\Data\DEVLANG_DATA_NEW';
+% STD_number = 1;
+
+%----------------------------------------------------------------------------------------------------
 
 % Load EEGLAB 
 % addpath(genpath('/Users/anne-sophiedubarry/Documents/4_Software/eeglab2020_0'));
@@ -112,18 +124,21 @@ DIFF_color = [0 0 0]; %black
     
     % Add channels information
     %EEG=pop_chanedit(EEG, 'lookup','/Users/anne-sophiedubarry/Documents/4_Software/eeglab2020_0/plugins/dipfit/standard_BEM/elec/standard_1005.elc');
-    EEG=pop_chanedit(EEG, 'lookup','\\Filer\\home\\Invites\\hervé\\Mes documents\\MATLAB\\eeglab2021.1\\plugins\\dipfit\\standard_BEM\\elec\\standard_1005.elc');
+    EEG=pop_chanedit(EEG, 'lookup',chan_dir);
 
     % Select DEV
     [EEG_DEV1,target_indices1] = pop_selectevent(EEG,'type','DEV1');
     [EEG_DEV2,target_indices2] = pop_selectevent(EEG,'type','DEV2');
     [EEG_STD,target_indices_std] = pop_selectevent(EEG,'type','STD');
     
-    %idx_std1 = target_indices_std(ismember(target_indices_std,target_indices1-1));
-    %idx_std2 = target_indices_std(ismember(target_indices_std,target_indices2-1));
-    begining_of_block = repelem((1:30:900)-1,3)+repmat(1:3,1,30); 
-    idx_std1 = setdiff(target_indices_std,begining_of_block);
-    idx_std2 = setdiff(target_indices_std,begining_of_block);
+    if STD_number == 1
+        idx_std1 = target_indices_std(ismember(target_indices_std,target_indices1-1));
+        idx_std2 = target_indices_std(ismember(target_indices_std,target_indices2-1));
+    elseif STD_number == 2
+        begining_of_block = repelem((1:30:900)-1,3)+repmat(1:3,1,30); 
+        idx_std1 = setdiff(target_indices_std,begining_of_block);
+        idx_std2 = setdiff(target_indices_std,begining_of_block);
+    end
     
     [EEG_STD1,target_indices_std1] = pop_selectevent(EEG,'event',idx_std1);
     [EEG_STD2,target_indices_std2] = pop_selectevent(EEG,'event',idx_std2);
@@ -133,10 +148,11 @@ DIFF_color = [0 0 0]; %black
     [EEG_DEV1_thresh,idx_dev1_rej] = pop_eegthresh(EEG_DEV1,1,elec ,rej_low, rej_high, win_of_interest(1), win_of_interest(2),0,1);
     [EEG_DEV2_thresh,idx_dev2_rej] = pop_eegthresh(EEG_DEV2,1,elec ,rej_low, rej_high, win_of_interest(1), win_of_interest(2),0,1);
    
-    %[EEG_STD_thresh, idx_removed] = pop_eegthresh(EEG_STD,1,elec ,rej_low, rej_high, win_of_interest(1), win_of_interest(2),0,1);
-   
-    %std_good = setdiff(1:900,target_indices_std(idx_removed)); 
-    %begining_of_block = repelem((1:30:900)-1,3)+repmat(1:3,1,30); 
+    if STD_number == 1
+        [EEG_STD_thresh, idx_removed] = pop_eegthresh(EEG_STD,1,elec ,rej_low, rej_high, win_of_interest(1), win_of_interest(2),0,1);
+        std_good = setdiff(1:900,target_indices_std(idx_removed)); 
+        begining_of_block = repelem((1:30:900)-1,3)+repmat(1:3,1,30); 
+    end
     
      %Export information about rejected trials
       % Get indices of the trials which were rejected (without messing around with the relative indices)
@@ -165,73 +181,137 @@ DIFF_color = [0 0 0]; %black
       %  Save this table into a csv file (use function writetable)
     writetable(list_trial_infos,fullfile(indir,subjects{jj},strcat(filename,'_low_',num2str(rej_low),'_high_',num2str(rej_high),'infos_trials.csv'))) ; 
 
-    %%
-    for cc=1:2 
+    if STD_number == 1
         
-        figure('Name',strcat('Subject :',subjects{jj},'Condition :',conditions{cc+1},'_allSTD'),'Units','normalized','Position',[0,0,1,1]);
+        % If nubmber of STD1 < number of DEV1 : randomly select other STD
+        if length(EEG_DEV1_thresh.event)>length(EEG_STD1_thresh.event)
+            % Find number of trial to add in STD
+            ntrials =  length(EEG_DEV1_thresh.event)-length(EEG_STD1_thresh.event) ;
+            %Apply function to balance number of STD trial to reach the number of DEV trials
+            EEG_STD1_thresh = balance_number_of_STD(EEG,ntrials,std_good,target_indices1,begining_of_block,target_indices_std1,idx_std1_rej,idx_std1) ;
+            
+        end
+        
+        % If nubmber of STD2 < number of DEV2 : randomly select other STD
+        if length(EEG_DEV2_thresh.event)>length(EEG_STD2_thresh.event)
+            % Find number of trial to add in STD
+            ntrials =  length(EEG_DEV2_thresh.event)-length(EEG_STD2_thresh.event) ;
+            %Apply function to balance number of STD trial to reach the number of DEV trials
+            EEG_STD2_thresh = balance_number_of_STD(EEG,ntrials,std_good,target_indices2,begining_of_block,target_indices_std2,idx_std2_rej,idx_std2) ;
+            
+        end
+        
+        % If nubmber of STD1 > number of DEV1 : delete random STD preceding missing DEV1
+        if length(EEG_DEV1_thresh.event)<length(EEG_STD1_thresh.event)
+            %Find indices of rejected DEV1 in the 900 trials referential
+            list_of_rej_dev1 = target_indices1(idx_dev1_rej);
+            %Select random indices amont the rejected DEV1
+            random_std1_to_remove = randsample(list_of_rej_dev1-1,size(idx_dev1_rej,2)-size(idx_std1_rej,2));
+            %Remove STD corresponding to rejected DEV1
+            idx_std_already_included = setdiff(target_indices_std1, target_indices_std1(idx_std1_rej)) ;
+            new_list_of_std1 = setdiff(idx_std_already_included,random_std1_to_remove);
+            %Modify EEG struct
+            [EEG_STD1_thresh,~] = pop_selectevent(EEG,'event',[new_list_of_std1]);
+        end
+        
+        % If nubmber of STD2 > number of DEV2 : delete random STD preceding
+        % missing DEV2
+        if length(EEG_DEV2_thresh.event)<length(EEG_STD2_thresh.event)
+            %Find indices of rejected DEV1 in the 900 trials referential
+            list_of_rej_dev2 = target_indices2(idx_dev2_rej);
+            %Select random indices amont the rejected DEV1
+            random_std2_to_remove = randsample(list_of_rej_dev2-1,size(idx_dev2_rej,2)-size(idx_std2_rej,2));
+            %Remove STD corresponding to rejected DEV1
+            idx_std_already_included = setdiff(target_indices_std2, target_indices_std2(idx_std2_rej)) ;
+            new_list_of_std2 = setdiff(idx_std_already_included,random_std2_to_remove);
+            %Modify EEG struct
+            [EEG_STD2_thresh,~] = pop_selectevent(EEG,'event',[new_list_of_std2]);
+        end
+        
+        
+    end
+   
+    for cc=1:2
+        
+        if STD_number == 1
+            figure('Name',strcat('Subject :',subjects{jj},'Condition :',conditions{cc+1}),'Units','normalized','Position',[0,0,1,1]);
+        elseif STD_number == 2
+            figure('Name',strcat('Subject :',subjects{jj},'Condition :',conditions{cc+1},'_allSTD'),'Units','normalized','Position',[0,0,1,1]);
+        end
         
         EEG_DEV = eval(sprintf('EEG_DEV%d_thresh',cc)) ;
         EEG_STD = eval(sprintf('EEG_STD%d_thresh',cc)) ;
-        nfig = 1 ; 
-
+        nfig = 1 ;
+        
         for elec_letter=1:size(elec_to_disp_labels,1)
-   
+            
             for elec_numb=1:size(elec_to_disp_labels,2)
-
-                hAxes = subplot(size(elec_to_disp_labels,1),size(elec_to_disp_labels,2),nfig) ; 
+                
+                hAxes = subplot(size(elec_to_disp_labels,1),size(elec_to_disp_labels,2),nfig) ;
                 nfig = nfig +1 ;
-
-                idx_elec = find(ismember({EEG_DEV.chanlocs.labels},elec_to_disp_labels(elec_letter,elec_numb))) ; 
-
+                
+                idx_elec = find(ismember({EEG_DEV.chanlocs.labels},elec_to_disp_labels(elec_letter,elec_numb))) ;
+                
                 % Compute grand average over one electrode
-                grd_STD = squeeze(mean(EEG_STD.data(idx_elec,:,:),3)) ; 
-                grd_DEV = squeeze(mean(EEG_DEV.data(idx_elec,:,:),3)) ; 
-                grd_DIFF = grd_DEV - grd_STD ; 
-
+                grd_STD = squeeze(mean(EEG_STD.data(idx_elec,:,:),3)) ;
+                grd_DEV = squeeze(mean(EEG_DEV.data(idx_elec,:,:),3)) ;
+                grd_DIFF = grd_DEV - grd_STD ;
+                
                 % Plot timeseries
-                plot(EEG_STD.times,grd_STD,'Color', STD_color,'Linewidth',1.5); hold on ;set(gca,'YDir','reverse') ; 
+                plot(EEG_STD.times,grd_STD,'Color', STD_color,'Linewidth',1.5); hold on ;set(gca,'YDir','reverse') ;
                 plot(EEG_STD.times,grd_DEV,'Color',DEV_colors{cc},'Linewidth',1.5);  hold on; set(gca,'YDir','reverse') ;
                 plot(EEG_STD.times,grd_DIFF,'Color',DIFF_color,'Linewidth',1.5);  hold on; set(gca,'YDir','reverse') ;
                 
-                % Plot transparetn halo (+-mad)
-                %plotHaloPatchMAD(hAxes, EEG_STD.times, squeeze(EEG_STD.data(idx_elec,:,:)), STD_color*255) ; 
+                % Plot transparent halo (+-mad)
+                %plotHaloPatchMAD(hAxes, EEG_STD.times, squeeze(EEG_STD.data(idx_elec,:,:)), STD_color*255) ;
                 %plotHaloPatchMAD(hAxes, EEG_DEV.times, squeeze(EEG_DEV.data(idx_elec,:,:)), DEV_colors{cc}*255);
-              
+                
                 % Adjust graphics
-                xlim([EEG_STD.xmin, EEG_STD.xmax]*1000); grid on ; 
-                %legend('STD (/DA/)',sprintf('DEV (/%s/)',cond_sylab{cc}),sprintf('DEV-STD (/%s/)',cond_sylab{cc}));
+                xlim([EEG_STD.xmin, EEG_STD.xmax]*1000); grid on ;
                 title(elec_to_disp_labels(elec_letter,elec_numb));
                 xlabel('Times (ms)'); ylabel('uV');
                 set(hAxes,'Fontsize',12);
-                % To save data in vectoriel
-                % print('-dsvg',fullfile(indir,strcat(subjects{jj},'_',conditions{cc+1},'.svg'))) ; 
                 
             end
-        
-        %Add a single legend for 6 plots
-        fig = gcf;
-        fig.Position(3) = fig.Position(3) + 250;
-        Lgnd = legend('STD (/DA/)',sprintf('DEV (/%s/)',cond_sylab{cc}),sprintf('DEV-STD (/%s/)',cond_sylab{cc}),'Location','bestoutside');
-        Lgnd.Position(1) = 0.06;
-        Lgnd.Position(2) = 0.8;
-        
-        %Add a single title for 6 plots
-        sgtitle([filename,' all STDs'],'Interpreter', 'None', 'Fontsize', 16, 'FontWeight', 'bold')
-                
-         % To save data in vectoriel and png
-         print('-dsvg',fullfile(indir,subjects{jj},strcat(subjects{jj},'_',conditions{cc+1},'_allSTD.svg')))
-         print('-djpeg',fullfile(indir,subjects{jj},strcat(subjects{jj},'_',conditions{cc+1},'_allSTD.jpg')))
-                   
+            
+            %Add a single legend for 6 plots
+            fig = gcf;
+            fig.Position(3) = fig.Position(3) + 250;
+            Lgnd = legend('STD (/DA/)',sprintf('DEV (/%s/)',cond_sylab{cc}),sprintf('DEV-STD (/%s/)',cond_sylab{cc}),'Location','bestoutside');
+            Lgnd.Position(1) = 0.06;
+            Lgnd.Position(2) = 0.8;
+            
+            %Add a single title for 6 plots
+            if STD_number == 1
+                sgtitle([filename,' balanced number of STDs'],'Interpreter', 'None', 'Fontsize', 16, 'FontWeight', 'bold');
+            elseif STD_number == 2
+                sgtitle([filename,' unbalanced number of STDs (all)'],'Interpreter', 'None', 'Fontsize', 16, 'FontWeight', 'bold')
+            end
+            
+            % To save data in vectoriel and png
+            if STD_number == 1
+                print('-dsvg',fullfile(indir,subjects{jj},strcat(subjects{jj},'_',conditions{cc+1},'_balancedSTD.svg')));
+                %print('-dpng',fullfile(indir,subjects{jj},strcat(subjects{jj},'_',conditions{cc+1},'_balancedSTD.png')));
+                print('-dpng',fullfile(plots_dir,strcat(subjects{jj},'_',conditions{cc+1},'_balancedSTD.png')));
+            elseif STD_number == 2
+                print('-dsvg',fullfile(indir,subjects{jj},strcat(subjects{jj},'_',conditions{cc+1},'_allSTD.svg')));
+                %print('-dpng',fullfile(indir,subjects{jj},strcat(subjects{jj},'_',conditions{cc+1},'_allSTD.png')));
+                print('-dpng',fullfile(plots_dir,strcat(subjects{jj},'_',conditions{cc+1},'_allSTD.png')));
+            end
+            
         end
-                
+        
         %Export data
         [ALLEEG, EEG_DEV, CURRENTSET] = pop_newset(ALLEEG, EEG_DEV, CURRENTSET, 'setname',strcat(filename,'_','DEV',num2str(cc)),'savenew', fullfile(indir,subjects{jj},strcat(filename,'_','DEV',num2str(cc))),'gui','off');
-        [ALLEEG, EEG_STD, CURRENTSET] = pop_newset(ALLEEG, EEG_STD, CURRENTSET, 'setname',strcat(filename,'_','all_STD',num2str(cc)),'savenew', fullfile(indir,subjects{jj},strcat(filename,'_','all_STD',num2str(cc))),'gui','off');
-            
+        if STD_number == 1
+            [ALLEEG, EEG_STD, CURRENTSET] = pop_newset(ALLEEG, EEG_STD, CURRENTSET, 'setname',strcat(filename,'_','STD',num2str(cc)),'savenew', fullfile(indir,subjects{jj},strcat(filename,'_','STD',num2str(cc))),'gui','off');
+        elseif STD_number == 2
+            [ALLEEG, EEG_STD, CURRENTSET] = pop_newset(ALLEEG, EEG_STD, CURRENTSET, 'setname',strcat(filename,'_','all_STD',num2str(cc)),'savenew', fullfile(indir,subjects{jj},strcat(filename,'_','all_STD',num2str(cc))),'gui','off');
+        end
+        
     end
 
-end
-end
+    end    
     
 %--------------------------------------------------------------
 % FUNCTION that select from EEG_STD ntrial with no repetition with exisitng
